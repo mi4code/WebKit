c3a90061065c9e57d919cb797ffd155176423aa0
mi4code <114027049+mi4code@users.noreply.github.com>
2024-10-11 15:15:54 +0200

    add libHUI test includes

--- Tools/libHUI/main.cc
+++ Tools/libHUI/main.cc
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#include <WebKit/WKRetainPtr.h>
+#include <WebKit/WebKit2_C.h>
+#include <unordered_map>
+#include <wtf/Ref.h>
+
 int main() {
     std::cout << "Hello, CMake!" << std::endl;
     return 0;
